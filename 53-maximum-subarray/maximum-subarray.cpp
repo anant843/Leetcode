27113dd1f140b6e0b2e8b0c1b36566b103d1e918
Anant Singh Shishodia <anantthakur2005@gmail.com>
2026-09-13 08:25:01 +0530

    Time: 4 ms (10.61%) | Memory: 71.7 MB (81.24%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,14 +1,57 @@
+// class Solution {
+// public:
+//     int maxSubArray(vector<int>& nums) {
+
+//         int n = nums.size();
+//         int maxi = INT_MIN;
+
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i; j < n; j++) {
+//                 int sum = 0;
+
+//                 for (int k = i; k <=j; k++) {
+//                     sum += nums[k];
+//                 }
+//                  maxi = max(sum, maxi);
+//             }
+//         }
+//      return maxi;
+//  }
+// };
+
+// class Solution {
+// public:
+//     int maxSubArray(vector<int>& nums) {
+//         int n = nums.size();
+//         int maxi = INT_MIN;
+
+//         for (int i = 0; i < n; i++) {
+//             int sum = 0;
+
+//             for (int j = i; j < n; j++) {
+//                 sum += nums[j];
+//                 maxi = max(maxi, sum);
+//             }
+//         }
+
+//         return maxi;
+//     }
+// };
+
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int currentSum = nums[0];
-        int maxSum = nums[0];
+        int sum = 0;
+        int maxi = nums[0];
 
-        for (int i = 1; i < nums.size(); i++) {
-            currentSum = max(nums[i], currentSum + nums[i]);
-            maxSum = max(maxSum, currentSum);
+        for (int x : nums) {
+            sum += x;
+            maxi = max(maxi, sum);
+
+            if (sum < 0)
+                sum = 0;
         }
 
-        return maxSum;
+        return maxi;
     }
 };
