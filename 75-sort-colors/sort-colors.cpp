c3ceaa1d9013ec04a273b1cc6dfb481dd85dbe1a
Anant Singh Shishodia <anantthakur2005@gmail.com>
2026-09-13 09:22:36 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (49.03%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,29 +1,56 @@
+// class Solution {
+// public:
+//     void sortColors(vector<int>& nums) {
+
+//       int countzero=0;
+//       int countone=0;
+//       int counttwo=0;
+
+//       for(int x:nums){
+//         if(x==0)countzero++;
+//         else if(x==1)countone++;
+//         else counttwo++;
+//       }
+
+//       int i=0;
+
+//       while(countzero--){
+//         nums[i++]=0;
+//       }
+
+//       while(countone--){
+//         nums[i++]=1;
+//       }
+
+//       while(counttwo--){
+//         nums[i++]=2;
+//       }
+//     }
+// };
+
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+        int low=0;
+        int mid=0;
+        int high=nums.size()-1;
 
-      int countzero=0;
-      int countone=0;
-      int counttwo=0;
+        while(mid<=high){
 
-      for(int x:nums){
-        if(x==0)countzero++;
-        else if(x==1)countone++;
-        else counttwo++;
-      }
+            if(nums[mid]==0){
+                swap(nums[mid],nums[low]);
+                low++;
+                mid++;
+            }
 
-      int i=0;
+            else if(nums[mid]==1){
+                mid++;
+            }
 
-      while(countzero--){
-        nums[i++]=0;
-      }
-
-      while(countone--){
-        nums[i++]=1;
-      }
-
-      while(counttwo--){
-        nums[i++]=2;
-      }
+            else{
+                swap(nums[mid],nums[high]);
+                high--;
+            }
+        }
     }
 };
